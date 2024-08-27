d69b84085b0bacbafecea851bbc34a246ea388c3
VitaliiS <svjfox@gmail.com>
2024-08-27 18:25:22 +0300

    2708

--- ContosoUniTARgv23/Controllers/StudentsController.cs
+++ ContosoUniTARgv23/Controllers/StudentsController.cs
@@ -1,4 +1,5 @@
 ﻿using ContosoUniTARgv23.Data;
+using ContosoUniTARgv23.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,6 +18,48 @@ namespace ContosoUniTARgv23.Controllers
         {
             var result = await _context.Students.ToListAsync();
             return View(result);
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .Include(s => s.Enrollments)
+                    .ThenInclude(e => e.Course)
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(m => m.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(student);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists, " +
+                    "see your system administrator.");
+            }
+
+            return View(student);
+
         }
     }
 }
