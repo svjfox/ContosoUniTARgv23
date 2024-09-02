1ed1b51568354c580d31cdb3e2457f9744b32cce
VitaliiS <svjfox@gmail.com>
2024-09-02 16:32:04 +0300

    0209

--- ContosoUniTARgv23/Controllers/StudentsController.cs
+++ ContosoUniTARgv23/Controllers/StudentsController.cs
@@ -14,10 +14,31 @@ namespace ContosoUniTARgv23.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string sortOrder)
         {
-            var result = await _context.Students.ToListAsync();
-            return View(result);
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+
+            var students = from s in _context.Students
+                           select s;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    students = students.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    students = students.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    students = students.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    students = students.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            return View();
         }
 
 
